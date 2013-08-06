8dc0ac90b4290f001113bdc7267e71513240a599
Sxw1212 <samlmauldin@gmail.com>
2013-08-06 18:46:26 -0500

    added getChunk, toAbs, and toRel to utils also reworking terrain sending

--- plugins/terrain.js
+++ plugins/terrain.js
@@ -1,13 +1,20 @@
 var chunk = require("../lib/world/chunk");
+var chunks = []
 
 module.exports = function(server) {
 	server.on("client_preconnect2", function(client) {
 		for (var x = 0; x < 14; x++) {
+			chunks[x] = chunks[x] || []
 			for (var z = 0; z < 14; z++) {
-				var cdata = chunk.build(x-7, z-7);
-				cdata.generate(server);
+				var cdata;
+				if (!chunks[x][y]) {
+					chunks[x][y] = chunk.build(x-7, z-7);
+					chunks[x][y].generate(server);
+				}
+				cdata = chunks[x][y];
 				cdata.send(client);
 			}
 		}
 	});
+	server.on("packet_
 };
